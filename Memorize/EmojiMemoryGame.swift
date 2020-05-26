56e4e6664bc471fa10cb264282fd04fbd0d7a545
Pablo Cornejo <pcornejo@icloud.com>
2020-05-26 13:08:43 -0400

    Add model and view model

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Pablo Cornejo on 5/26/20.
+//  Copyright © 2020 Pablo Cornejo Pierola. All rights reserved.
+//
+
+import SwiftUI
+
+/// The view model for the content view.
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    // MARK: - Access to the model
+    
+    var cards: [MemoryGame<String>.Card] {
+        model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,34 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Pablo Cornejo on 5/26/20.
+//  Copyright © 2020 Pablo Cornejo Pierola. All rights reserved.
+//
+
+import Foundation
+
+/// The game model.
+struct MemoryGame<CardContent> {
+    var cards: [Card]
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = []
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
+        }
+    }
+    
+    func choose(card: Card) {
+        print("Card chosen: \(card)")
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
+        var id: Int
+    }
+}
