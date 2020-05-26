615a2330622f4496ed932cbbae4f914b1c06cf13
Pablo Cornejo <pcornejo@icloud.com>
2020-05-26 11:38:44 -0400

    Initial commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Pablo Cornejo on 5/26/20.
+//  Copyright © 2020 Pablo Cornejo Pierola. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10).fill()
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
