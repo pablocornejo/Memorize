af598d28e4cde21d2f5bf13ba64a187502a4058a
Pablo Cornejo <pcornejo@icloud.com>
2020-05-26 22:15:11 -0400

    Add Grid and GridLayout

---
+++ Memorize/Grid.swift
@@ -0,0 +1,44 @@
+//
+//  Grid.swift
+//  Memorize
+//
+//  Created by Pablo Cornejo on 5/26/20.
+//  Copyright © 2020 Pablo Cornejo Pierola. All rights reserved.
+//
+
+import SwiftUI
+
+struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
+    var items: [Item]
+    var viewForItem: (Item) -> ItemView
+    
+    init(_ items: [Item], viewForItem: @escaping (Item) -> ItemView) {
+        self.items = items
+        self.viewForItem = viewForItem
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            self.body(for: GridLayout(itemCount: self.items.count, in: geometry.size))
+        }
+    }
+    
+    func body(for layout: GridLayout) -> some View {
+        ForEach(items) { item in
+            self.body(for: item, in: layout)
+        }
+    }
+    
+    func body(for item: Item, in layout: GridLayout) -> some View {
+        let index = items.firstIndex(matching: item)
+        return viewForItem(item)
+            .frame(width: layout.itemSize.width, height: layout.itemSize.height)
+            .position(layout.location(ofItemAt: index))
+    }
+}
+
+extension Array where Element: Identifiable {
+    func firstIndex(matching: Element) -> Int {
+        firstIndex { $0.id == matching.id } ?? 0
+    }
+}
